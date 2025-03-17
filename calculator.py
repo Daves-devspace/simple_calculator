7ec68c1020c76d3c62f0c82a84464fa280e706f1
Daves-devspace <126021050+Daves-devspace@users.noreply.github.com>
2025-03-17 17:33:29 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,27 @@
+#user input
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+operation= input("Enter the operation (+,-,/,*): ")
 
+#calculations
+if operation == "+":
+  result = num1 + num2
+  print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+  result = num1 - num2
+  print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+  result = num1 * num2
+  print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+  if num2 != 0:
+    result = num1 / num2
+    print(f"{num1} / {num2} = {result}")
+  else:
+    print("Error:Number cann't be divisible by zero.")
+else:
+  print("Please enter a valid operation +,-,/,*.")
+  
+print("Enjoy the calculations")
+    
+  
